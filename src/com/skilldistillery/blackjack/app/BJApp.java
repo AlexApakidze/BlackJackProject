847bc3657b60ac1c02294cf7ef6d4afc44c8c443
Alex A <Alexapakidze@yahoo.com>
2023-06-09 14:28:25 -0600

    Created starter class

---
+++ src/com/skilldistillery/blackjack/app/BJApp.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.blackjack.app;
+
+public class BJApp {
+	public static void main(String[] args) {
+		BJApp bj = new BJApp();
+		
+	}
+
+	public void run() {
+
+	}
+
+}
---
+++ src/com/skilldistillery/blackjack/entities/BlackJackHand.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.blackjack.entities;
+
+public class BlackJackHand extends Hand {
+
+	@Override
+	public int geteHandValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
